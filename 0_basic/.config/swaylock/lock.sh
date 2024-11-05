7643a475a548c210559c2c6f4ff05d2f23adf524
Z0SO <lautarorobales31@gmail.com>
2024-11-05 11:37:04 -0300

    modified:0_basic/.config/hypr/config/keybinds.conf

---
+++ 0_basic/.config/swaylock/lock.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+swaylock \
+    --screenshots \
+    --clock \
+    --indicator \
+    --indicator-radius 100 \
+    --indicator-thickness 7 \
+    --effect-blur 7x5 \
+    --ring-color 282828 \
+    --key-hl-color ebdbb2 \
+    --line-color 00000000 \
+    --inside-color 00000088 \
+    --separator-color 00000000 \
+    --text-color ffffff \
+    --fade-in 0.2
