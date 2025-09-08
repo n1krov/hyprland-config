7b254f6e155272497bf3688d1da7a38b4aba74b1
Z0SO <lautarorobales31@gmail.com>
2025-09-08 00:23:15 -0300

    arreglando errores, faLTA MUCHO TODAVAI

---
+++ dot_config/hypr/scripts/autostart.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# autostart.sh - Script para lanzar programas y servicios personalizados al iniciar Hyprland
+
+# Lanzar Discord en segundo plano
+# discord &
+
+# Lanzar Telegram en segundo plano
+# telegram-desktop &
+
+# Lanzar Spotify
+# spotify &
+
+# Lanzar cualquier otro programa personalizado
+# ejemplo_programa &
+
+# Puedes agregar comandos, variables o scripts propios aquí.
+
+exit 0
